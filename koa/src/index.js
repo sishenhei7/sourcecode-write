31e9689f165623bf2a0ac631551be4cfdadaf096
yangzhou <zhou.yang@klook.com>
2021-08-09 09:19:54 +0800

    chore: delete old koa project

--- koa/src/index.js
+++
@@ -1,6 +0,0 @@
-require('./test.js')
-const debug = require('debug')('koa:application')
-debug('index')
-setTimeout(() => {
-  debug('index 1s')
-}, 1000);
---
+++ koa/src/index.ts
@@ -0,0 +1,6 @@
+// require('./test.js')
+// const debug = require('debug')('koa:application')
+// debug('index')
+// setTimeout(() => {
+//   debug('index 1s')
+// }, 1000);
